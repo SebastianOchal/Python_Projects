e1025e63122c1f74ad087531551b67350926975e
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2024-12-05 16:03:05 +0000

    Created saperated code in Display_weather to make code modular. In addition managing user inputs which are invalid

--- Weather_App/Weather_App.py
+++ Weather_App/Weather_App.py
@@ -4,18 +4,21 @@ weather_data = {
     "Berlin":{"Temperature":"5°C", "Conditions":"Partly Sunny","Wind_Speed":"11km/h"},
     "Dublin":{"Temperature":"10°C", "Conditions":"Cloudy","Wind_Speed":"17km/h"},
     "Madrid":{"Temperature":"9°C", "Conditions":"Sunny","Wind_Speed":"3km/h"}
-}
-
-def Display_weather():
+    }
+    
+def menu():
     print(50*"=")
     print("Here are the avalible cities, Type their name to get the weather data")
     for data in weather_data:
         print(data)
     print(50*"=")
+    display_weather()
+
+def display_weather():
     user_request = input("What City would you like the weather for?:")
     location = user_request.lower()
-
-    for  city in weather_data:
+    city_found = False
+    for city in weather_data:
         if location == city.lower():
             print(50*"=")
             print(f"City: {city}")
@@ -24,12 +27,21 @@ def Display_weather():
             print(f"Conditions:  |{weather_data[city]["Conditions"]}|")
             print(f"Wind Speed:  |{weather_data[city]["Wind_Speed"]}|")
             print(50*"=")
-    
-    check_again = input("Would you like to check a Different City? Yes or No: ")
+            leave()
+            city_found = True
+    if city_found == False:
+        print("this city does not exist")
+        menu()
+        
+def leave(Exit_message = "Would you like to Exit? Yes or No: "):
+    check_again = input(Exit_message)
     answer = check_again.lower()
     if answer == "yes":
-        Display_weather()
-    else:
         print("ok bye")
-
-Display_weather()
+    elif answer == "no":
+        menu()
+    else:
+        print("That was not an option please try again")
+        leave()
+    
+menu()
